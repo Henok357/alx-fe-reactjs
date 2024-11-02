a89b0d8ef836c9c50bb8aa390af1a511a3db8f02
Henok <henokbelete357@gmail.com>
2024-11-02 13:28:53 -0700

    Your commit message here

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,12 +6,13 @@ import Footer from './Footer';
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
       <MainContent />
       <Footer />
-    </div>
+    </>
   );
 }
 
 export default App;
+
