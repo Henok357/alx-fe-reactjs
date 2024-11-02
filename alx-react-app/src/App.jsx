c2a92e4d99706a80c9042ad38c20ce17bf709ee0
Henok <henokbelete357@gmail.com>
2024-11-02 13:56:28 -0700

    Create UserProfile component and use it in App

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,16 +1,13 @@
-import React from 'react';
-import Header from './Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
+import UserProfile from './components/UserProfile';
 
+import React from 'react';
 function App() {
   return (
     <>
-      <Header />
-      <MainContent />
-      <Footer />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     </>
   );
 }
+
 
 export default App;
