ba1c252456d7a190bbc0b8d4690e7dca508b331d
Henok <henokbelete357@gmail.com>
2024-11-08 09:41:12 -0800

    Added inline CSS styling to React components

---
+++ alx-react-app-new/src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: 'black', color: 'white', textAlign: 'center', padding: '10px', marginTop: '20px' }}>
+      <p>© 2024 My Favorite Cities</p>
+    </footer>
+  );
+}
+
+export default Footer;
