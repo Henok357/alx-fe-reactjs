db8707aa38555450368b57333a847b4a84e06895
Henok <henokbelete357@gmail.com>
2024-11-09 18:59:17 -0800

    Complete Task 3: Implement React routing, Navbar, Footer, and contact form

---
+++ my-company/src/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+      <footer style={{ padding: '10px', backgroundColor: '#eee', textAlign: 'center' }}>
+        &copy; 2024 My Company
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
